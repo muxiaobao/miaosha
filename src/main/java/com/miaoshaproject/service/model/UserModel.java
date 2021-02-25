05e8115a16fd4f7c27348cd113c2d85fd937fcdf
muxiaobao <995652331@qq.com>
2021-02-25 16:09:46 +0800

    git commit -m "First commit"

---
+++ src/main/java/com/miaoshaproject/service/model/UserModel.java
@@ -0,0 +1,82 @@
+package com.miaoshaproject.service.model;
+
+/**
+ * MVC架构中，UserDO、UserPasswordDO等data object对应数据库的表信息，是纯粹的ORM映射，不涉及业务逻辑的处理
+ * 而在service层相关方法中，不可以直接返回查询到的data object对象，应该使用Model进一步封装
+ * 【在该类中，将encryptPassword字段也封装到UserModel中】
+ */
+public class UserModel {
+    private Integer id;
+    private String name;
+    private Byte gender;
+    private Integer age;
+    private String telephone;
+    private String registerMode;
+    private String thirdPartyId;
+
+    private String encryptPassword;
+
+    public String getEncryptPassword() {
+        return encryptPassword;
+    }
+
+    public void setEncryptPassword(String encryptPassword) {
+        this.encryptPassword = encryptPassword;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Byte getGender() {
+        return gender;
+    }
+
+    public void setGender(Byte gender) {
+        this.gender = gender;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getRegisterMode() {
+        return registerMode;
+    }
+
+    public void setRegisterMode(String registerMode) {
+        this.registerMode = registerMode;
+    }
+
+    public String getThirdPartyId() {
+        return thirdPartyId;
+    }
+
+    public void setThirdPartyId(String thirdPartyId) {
+        this.thirdPartyId = thirdPartyId;
+    }
+}
