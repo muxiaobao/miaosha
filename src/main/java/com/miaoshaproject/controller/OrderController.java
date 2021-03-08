f1aec7ecd61a244f78f46377c0e015c880dd7af6
muxiaobao <995652331@qq.com>
2021-03-08 23:41:37 +0800

    use token to achieve distributed session

--- src/main/java/com/miaoshaproject/controller/OrderController.java
+++ src/main/java/com/miaoshaproject/controller/OrderController.java
@@ -6,7 +6,9 @@ import com.miaoshaproject.response.CommonReturnType;
 import com.miaoshaproject.service.OrderService;
 import com.miaoshaproject.service.model.OrderModel;
 import com.miaoshaproject.service.model.UserModel;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,6 +27,9 @@ public class OrderController extends BaseController {
     @Autowired
     private HttpServletRequest httpServletRequest;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
 
 
     // 封装下单请求
@@ -33,12 +38,16 @@ public class OrderController extends BaseController {
     public CommonReturnType createOrder(@RequestParam(name = "itemId")Integer itemId,
                                         @RequestParam(name = "promoId", required = false)Integer promoId,
                                         @RequestParam(name = "amount")Integer amount) throws BusinessException {
-        // 获取用户的登录信息
-        Boolean isLogin = (Boolean) this.httpServletRequest.getSession().getAttribute("IS_LOGIN");
-        if (isLogin == null || !isLogin.booleanValue()){
+        // 获取用户的登录信息 【改用token+redis】
+        String token = httpServletRequest.getParameterMap().get("token")[0];
+        if (StringUtils.isEmpty(token)) {
             throw new BusinessException(EmBusinessError.USER_NOT_LOGIN, "用户还未登录，不能下单");
         }
-        UserModel userModel = (UserModel) this.httpServletRequest.getSession().getAttribute("LOGIN_USER");
+        UserModel userModel = (UserModel) redisTemplate.opsForValue().get(token);
+        if (userModel == null) {
+            // 会话过期等原因
+            throw new BusinessException(EmBusinessError.USER_NOT_LOGIN,"用户还未登录，不能下单");
+        }
 
         // 创建订单
         OrderModel orderModel = orderService.createOrder(userModel.getId(), itemId, promoId, amount);
--- src/main/java/com/miaoshaproject/controller/UserController.java
+++ src/main/java/com/miaoshaproject/controller/UserController.java
@@ -10,6 +10,7 @@ import com.miaoshaproject.service.model.UserModel;
 import org.apache.tomcat.util.security.MD5Encoder;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import sun.misc.BASE64Encoder;
@@ -19,6 +20,8 @@ import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Random;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 
 @Controller("user")
@@ -31,6 +34,9 @@ public class UserController extends BaseController {
 
     @Autowired
     private HttpServletRequest httpServletRequest;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
 
 
 
@@ -47,11 +53,16 @@ public class UserController extends BaseController {
         // 用户登录服务，校验用户登陆是否合法
         UserModel userModel = userService.validateLogin(telephone, this.EncodeByMd5(password));
 
-        // 将登陆凭证加入到用户登陆成功的session内
-        this.httpServletRequest.getSession().setAttribute("IS_LOGIN",true);
-        this.httpServletRequest.getSession().setAttribute("LOGIN_USER", userModel);
+        // 基于token实现用户态的管理，用户登陆验证成功后，将登录信息和登陆凭证一起存入redis
+        // 1.生成登陆凭证token
+        String uuidToken = UUID.randomUUID().toString();
+        uuidToken = uuidToken.replace("-", "");
+        // 2.建立token和用户登录态之间的联系
+        redisTemplate.opsForValue().set(uuidToken, userModel);
+        redisTemplate.expire(uuidToken,1, TimeUnit.HOURS);
 
-        return CommonReturnType.create(null);
+        // 3.下发token
+        return CommonReturnType.create(uuidToken);
     }
 
 
