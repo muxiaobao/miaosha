a4da09125526f41128ac8dbf490eb6c4787784a6
muxiaobao <995652331@qq.com>
2021-03-09 18:54:55 +0800

    self-define RedisTemplate, serializer and deserializer methods added, in order to cache item detail page

--- src/main/java/com/miaoshaproject/config/RedisConfig.java
+++ src/main/java/com/miaoshaproject/config/RedisConfig.java
@@ -1,5 +1,15 @@
 package com.miaoshaproject.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.miaoshaproject.serializer.JodaDateTimeJsonDeserializer;
+import com.miaoshaproject.serializer.JodaDateTimeJsonSerializer;
+import org.joda.time.DateTime;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.stereotype.Component;
 
@@ -9,5 +19,35 @@ import org.springframework.stereotype.Component;
 @Component
 @EnableRedisHttpSession(maxInactiveIntervalInSeconds = 3600)
 public class RedisConfig {
+
+    @Bean
+    public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate redisTemplate = new RedisTemplate();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+        // 1.首先解决key的序列化方式（key是string类型）
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        redisTemplate.setKeySerializer(stringRedisSerializer);
+
+        // 2. 然后解决value的序列化方式 （json方式）
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        // 定制化datetime类型关于json的序列化和反序列化操作
+        ObjectMapper objectMapper = new ObjectMapper();
+        SimpleModule simpleModule = new SimpleModule();
+        simpleModule.addSerializer(DateTime.class, new JodaDateTimeJsonSerializer());
+        simpleModule.addDeserializer(DateTime.class, new JodaDateTimeJsonDeserializer());
+
+        //  设置自动包含类型信息，使得json串可以反序列化成相应的类型
+        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+
+        objectMapper.registerModule(simpleModule);
+        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+
+
+        return redisTemplate;
+    }
+
 
 }
